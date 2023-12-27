d3d7c53050c9817aceb3858309bbfd0d76b71c44
Piyush Raisinghani <piyush2521@gmail.com>
2023-12-27 16:09:35 -0600

    Made some changes

--- wallpaper_script.py
+++ wallpaper_script.py
@@ -2,27 +2,21 @@
 import urllib.request
 import requests
 import os
-# from  pprint import pprint
 from dotenv import load_dotenv
-# from PIL import Image
 import ctypes
 
 load_dotenv()
 api_key = os.getenv("ACCESS_KEY")
 api_secret = os.getenv("SECRET_KEY")
+directory = os.getenv("FILE_SAVE_DIRECTORY")
 
 orientation = "landscape"
 count = 1
 
-res = requests.get(f"https://api.unsplash.com/photos/random/?orientation={orientation}&client_id={api_key}&count={count}").json()
+res = requests.get(f"https://api.unsplash.com/photos/random/?orientation={orientation}&client_id={api_key}&count={count}", timeout = 10).json()
 
-# pprint(res[0], indent= 4, depth= 3)
 
 wallpaper_url = res[0]["links"]["download"]
-# wallpaper_url
 urllib.request.urlretrieve(wallpaper_url, "wallpaper.png")
 
-# img = Image.open(r"wallpaper.png")
-# img.show()
-
-ctypes.windll.user32.SystemParametersInfoW(20, 0, "D:\OneDrive\Practice\Wallpaper App\wallpaper.png" , 0)
+ctypes.windll.user32.SystemParametersInfoW(20, 0, f"{directory}wallpaper.png" , 0)
